4080399dc763757590b2d5d298fe1f5257930694
Luke Stern <luke.stern@softwire.com>
2021-06-14 14:45:51 +0100

    Formatting clean up

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,10 @@
-import { getByRole, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import { getImageUrls, brokenImages } from './photo-viewer-component/getImageUrls';
 import { GetBigPhoto } from './photo-viewer-component/GetBigPhoto';
 import App from './App';
 import { ThumbNails } from './photo-viewer-component/ThumbNails';
 import renderer from 'react-test-renderer';
-
 
 // Unit tests
 test('renders react photo viewer heading', () => {
@@ -14,10 +13,11 @@ test('renders react photo viewer heading', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-
 test('image urls are correctly generated', () => {
   const imageUrls = getImageUrls();
-  const brokenUrls = brokenImages.map((imageNumber) => `https://picsum.photos/id/6${imageNumber}/1600/900.jpg`);
+  const brokenUrls = brokenImages.map(
+    (imageNumber) => `https://picsum.photos/id/6${imageNumber}/1600/900.jpg`
+  );
   imageUrls.forEach(
     (url) => expect(!brokenUrls.includes(url))
   );
@@ -29,12 +29,12 @@ test('image is selected when it is clicked', () => {
   const testedUrl = getImageUrls()[0];
   const testedImage = screen.getByAltText(testedUrl);
   userEvent.click(testedImage);
-  expect(testedImage.className==='small-photo-selected');  
+  expect(testedImage.className === 'small-photo-selected');
 
   const testedUrl1 = getImageUrls()[10];
   const testedImage1 = screen.getByAltText(testedUrl1);
   userEvent.click(testedImage1);
-  expect(testedImage1.className==='small-photo-selected'); 
+  expect(testedImage1.className === 'small-photo-selected');
 });
 
 test('large image is displayed if a thumbnail is selected', () => {
@@ -45,23 +45,23 @@ test('large image is displayed if a thumbnail is selected', () => {
 
   userEvent.click(testedImage);
 
-  expect(testedImage.className==='large-photo');
+  expect(testedImage.className === 'large-photo');
 });
 
 test('regression test for big photo', () => {
   const photoUrl = "Hello"
-  render(<GetBigPhoto photoSelectedUrl={photoUrl} setPhotoSelected={() => {}} />);
+  render(<GetBigPhoto photoSelectedUrl={photoUrl} setPhotoSelected={() => { }} />);
 
   const bigPhoto = screen.getByTestId('large-photo');
 
   expect(bigPhoto).toBeInTheDocument();
-  expect(bigPhoto.src===photoUrl);
-  expect(bigPhoto.className==='large-photo');
+  expect(bigPhoto.src === photoUrl);
+  expect(bigPhoto.className === 'large-photo');
 });
 
 test('regression test for thumbnails', () => {
   const photoUrl = "Hello"
-  render(<ThumbNails photoSelectedUrl={photoUrl} setPhotoSelected={() => {}} />)
+  render(<ThumbNails photoSelectedUrl={photoUrl} setPhotoSelected={() => { }} />)
 
   const smallPhotoContainer = screen.getByTestId('small-photo-container');
 
@@ -69,10 +69,13 @@ test('regression test for thumbnails', () => {
   expect(smallPhotoContainer.children).toHaveLength(44);
 });
 
-
 // Snapshot tests
 describe("PhotoViewer", () => {
-  let tree = renderer.create(<App />)
+  let tree;
+
+  beforeEach(() => {
+    tree = renderer.create(<App />)
+  })
 
   it("should match snapshots without selected photo", async () => {
     expect(tree.toJSON()).toMatchSnapshot();
@@ -82,15 +85,15 @@ describe("PhotoViewer", () => {
     renderer.act(() => {
       tree.update(<GetBigPhoto photoSelectedUrl={null} setPhotoSelected={null} />)
     });
-  
+
     expect(tree.toJSON()).toMatchSnapshot();
   });
 
   it("GetBigPhoto should match snapshots with selected photo", async () => {
     renderer.act(() => {
-      tree.update(<GetBigPhoto photoSelectedUrl={"Test"} setPhotoSelected={() => {}} />)
+      tree.update(<GetBigPhoto photoSelectedUrl={"Test"} setPhotoSelected={() => { }} />)
     });
-  
+
     expect(tree.toJSON()).toMatchSnapshot();
   });
 
@@ -98,15 +101,15 @@ describe("PhotoViewer", () => {
     renderer.act(() => {
       tree.update(<ThumbNails photoSelectedUrl={null} setPhotoSelected={null} />)
     });
-  
+
     expect(tree.toJSON()).toMatchSnapshot();
   });
 
   it("ThumbNails should match snapshots with selected photo", async () => {
     renderer.act(() => {
-      tree.update(<ThumbNails photoSelectedUrl={"Test"} setPhotoSelected={() => {}} />)
+      tree.update(<ThumbNails photoSelectedUrl={"Test"} setPhotoSelected={() => { }} />)
     });
-  
+
     expect(tree.toJSON()).toMatchSnapshot();
   });
 });
